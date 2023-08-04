9710bbac8eed3415b2350be08bed8ad0b1875d4f
passyswatz <passyswatz@gmail.com>
2023-08-04 01:11:08 +0100

    up to date

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import MovieCard from './MovieCard';
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=63654fc3';
 
- const movie1 =   {
-        "Title": "Italian Spiderman",
-        "Year": "2007",
-        "imdbID": "tt2705436",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
-    }
+//  const movie1 =   {
+//         "Title": "Italian Spiderman",
+//         "Year": "2007",
+//         "imdbID": "tt2705436",
+//         "Type": "movie",
+//         "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
+//     }
     
 
 const App = () => {
